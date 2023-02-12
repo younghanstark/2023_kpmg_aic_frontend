32523fe63239da084e351f05164f3521ad68fd20
younghan <younghanpark@yonsei.ac.kr>
2023-02-12 22:28:40 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,17 +7,27 @@ function App() {
   const [chats, setChats] = useState([]);
   const [disabled, setDisabled] = useState(false);
 
-  function query(input) {
+  const query = async (input) => {
     const newMsg = { type: "message", text: input };
     setChats([...chats, newMsg]);
+    setDisabled(true);
+
+    const response = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
+      )
+    ).json();
     setChats((prevState) => {
-      const newRes = { type: "response", text: "Sample response" };
+      const newRes = {
+        type: "response",
+        text: response.data.movies[0].summary.slice(0, 3),
+      };
       return [...prevState, newRes];
     });
-  }
+    setDisabled(false);
+  };
 
   const convRef = useRef();
-
   const scrollToBottom = () => {
     if (convRef.current)
       convRef.current.scrollTop = convRef.current.scrollHeight;
